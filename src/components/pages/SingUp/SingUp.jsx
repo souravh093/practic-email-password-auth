2721a778de67586df2bee2f5e05b4483961f5285
Sourav halder <souravh093@gmail.com>
2023-04-14 22:46:34 +0600

    project done

--- src/components/pages/SingUp/SingUp.jsx
+++ src/components/pages/SingUp/SingUp.jsx
@@ -1,59 +1,76 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, updateProfile } from "firebase/auth"
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  updateProfile,
+} from "firebase/auth";
 import app from "../../../Firebase/Firebase";
 
-const auth = getAuth(app)
-
-
+const auth = getAuth(app);
 
 const SingUp = () => {
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-    const [error, setError] = useState('')
-    const [success, setSuccess] = useState('')
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log('hello')
-        const name = event.target.username.value
-        const email = event.target.email.value
-        const password = event.target.password.value
-        if(!/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
-            setError('Please enter strong password')
-            return
-        }
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(result => {
-                const loggedInUser = result.user
-                profileUpdate(loggedInUser, name)
-                emailVerificationSend(loggedInUser)
-                console.log(result.user)
-            })
-            .catch(error => {
-                setError(error.message)
-            })
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("hello");
+    const name = event.target.username.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    if (
+      !/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+        password
+      )
+    ) {
+      setError("Please enter strong password");
+      return;
     }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const loggedInUser = result.user;
+        profileUpdate(loggedInUser, name);
+        emailVerificationSend(loggedInUser);
+        console.log(result.user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
 
-    const profileUpdate = (user, name) => {
-        updateProfile(user, {
-            displayName: name,
-        }).then(() => {
-            console.log('Updated profile')
-        }).catch(error => {
-            console.log('error', error.message)
-        })
-    }
+  const profileUpdate = (user, name) => {
+    updateProfile(user, {
+      displayName: name,
+    })
+      .then(() => {
+        console.log("Updated profile");
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
 
-    const emailVerificationSend = (user) => {
-        sendEmailVerification(user)
-            .then(() => {
-                setSuccess('Check your email')
-            })
-    }
+  const emailVerificationSend = (user) => {
+    sendEmailVerification(user).then(() => {
+      setSuccess("Check your email");
+    });
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <form onSubmit={handleSubmit} className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
         <h2 className="text-2xl font-bold mb-4">Register</h2>
         <div className="mb-4">
           <label
@@ -84,7 +101,7 @@ const SingUp = () => {
             required
           />
         </div>
-        <div className="mb-4">
+        <div className="mb-4 relative">
           <label
             className="block text-gray-700 font-bold mb-2"
             htmlFor="password"
@@ -95,13 +112,55 @@ const SingUp = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             name="password"
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Enter your password"
             required
           />
+          <button onClick={handleTogglePassword}>
+            {showPassword ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 absolute right-3 cursor-pointer bottom-1"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 absolute right-3 cursor-pointer bottom-1"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            )}
+          </button>
         </div>
         <div className="flex items-center justify-between">
-          <input type="submit" value="Submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" />
+          <input
+            type="submit"
+            value="Submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          />
           <div>
             <Link
               to="/login"
@@ -111,8 +170,8 @@ const SingUp = () => {
             </Link>
           </div>
         </div>
-      <p className="text-red-500 mt-5">{error}</p>
-      <p className="text-sky-500 mt-5">{success}</p>
+        <p className="text-red-500 mt-5">{error}</p>
+        <p className="text-sky-500 mt-5">{success}</p>
       </form>
     </div>
   );
